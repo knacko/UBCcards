f3cd5905e98ca5727c377eea016a4b7f5638882d
knacko <geekbike@hotmail.com>
2017-08-03 15:28:49 -0700

    Refactored the course sorting
    More abstract, and properly labels when collection is empty

--- client/views/courseList/courseList.js
+++ client/views/courseList/courseList.js
@@ -22,3 +22,30 @@ Template.courseList.events({
   },
 
 });
+Template.courseList.helpers ({
+	
+	course() {
+	var c = Courses.find({},{sort:{name: 1}});
+	console.log("Found courses: " + c.count());	
+		return c;
+	},
+	
+	notEnrolledCourse() {
+		var c = Courses.find({students: { $not: Meteor.userId()}},{sort:{name: 1}});
+		console.log("Found courses: " + c.count());	
+		return c;
+	},	
+	
+	enrolledCourse() {
+		var c = Courses.find({students: Meteor.userId()}, {sort:{name: 1}});
+		console.log("Found enrolled courses: " + c.count());	
+		return c;
+	},
+
+	enrolledInACourse(){
+		var n = Courses.find({students: Meteor.userId()}).count();	
+		console.log("User is enrolled in " + n + " courses.");
+		return n != 0;
+		
+	},
+});
--- client/views/courseList/courseRowTable.js
+++ client/views/courseList/courseRowTable.js
@@ -1,37 +1,10 @@
 
 import './courseRowTable';
 
-Template.courseRow.onCreated(function() {
-  Meteor.subscribe('courses');
-});
-
-
 Template.courseRowTable.helpers ({
-	course() {
-	var c = Courses.find({},{sort:{name: 1}});
-		console.log("Found courses: " + c.count());	
-		return c;
+	
+	nonEmpty: function(collection) {
+		return collection.count() != 0;		
 	},
-	
-	notEnrolledCourse() {
-		var c = Courses.find({},{sort:{name: 1}});
-		console.log("Found courses: " + c.count());	
-		return c;
-	},
-	
-	
-	
-	enrolledCourse() {
-		var c = Courses.find({students: Meteor.userId()}, {sort:{name: 1}});
-		console.log("Found enrolled courses: " + c.count());	
-		return c;
-	},
-
-	enrolledInACourse(){
-		var n = Courses.find({students: Meteor.userId()}).count();	
-		console.log("User is enrolled in " + n + " courses.");
-		return n != 0;
-		
-	}
 	
 });
